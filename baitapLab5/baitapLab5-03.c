664e18c1a2014c9c0b2b72c8f6e1d0145dd6a150
bui-hoang <gangtergamingx@gmail.com>
2024-04-19 11:01:43 +0700

    lab5
    Bài 3

--- baitapLab5/baitapLab5-03.c
+++ baitapLab5/baitapLab5-03.c
@@ -4,11 +4,34 @@
 
 #include <stdio.h>
 
-int baitapLab5_03() {
-//int main() {
-    int a = 0, b, c;
-    printf("Nhập một số bất kì:\n");
-    scanf("%d", &a);
+//int baitapLab5_03() {
+int main() {
+    int height, i, j;
+    do {
+        printf("Nhập vào chiều cao của tam giác:\n");
+        scanf("%d", &height);
+    } while (height < 2);
+
+    printf("\n");
+    for (i = 1; i <= height; i++) {
+        for (j = 1; j <= i; j++){
+            printf("%c", '*');
+            printf("\n");
+        }
+    }
+
+    printf("\n");
+    i = 1;
+    while (i <= height){
+        j = 1;
+        while (j <= 1){
+            printf("%c", '*');
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+
 
     return 0;
 }
